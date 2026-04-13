d093260bed1c3cf7efedff3c9fa4305b5541016f
Malicious Bunny~ <ngohgeorge.mbahnji@ictuniversity.edu.cm>
2026-04-13 13:03:40 +0100

    added tawk.to fxed

--- src/components/public/ChatWidget.tsx
+++ src/components/public/ChatWidget.tsx
@@ -4,22 +4,16 @@ import Script from 'next/script';
 
 export default function ChatWidget() {
   return (
-    <Script
-      id="tawk-to"
-      strategy="lazyOnload"
-      dangerouslySetInnerHTML={{
-        __html: `
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/69dcd5d65a78fd1c363996ae/1jm3a6k5m';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-          })();
-        `,
-      }}
-    />
+    <>
+      <Script id="tawk-init" strategy="afterInteractive">
+        {`window.Tawk_API = window.Tawk_API || {}; window.Tawk_LoadStart = new Date();`}
+      </Script>
+      <Script
+        id="tawk-to"
+        src="https://embed.tawk.to/69dcd5d65a78fd1c363996ae/1jm3a6k5m"
+        strategy="afterInteractive"
+        crossOrigin="*"
+      />
+    </>
   );
 }
